1967bf91f3c4533c2a7831252cff7ca7bb8314e0
Kikuo Emoto <emoto.kc@om.asahi-kasei.co.jp>
2020-11-13 22:54:16 +0900

    feat(js): separate identity
    - Identity parameters are set with a new function `setIdentity` instead
      of the `connect` function.

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,57 @@
 import { NativeModules } from 'react-native';
+// no type declaration for EventEmitter
+// @ts-ignore
+import EventEmitter from 'react-native/Libraries/vendor/emitter/EventEmitter';
 
 const { MqttClient: MqttClientImpl } = NativeModules;
 
 /**
  * MQTT client.
+ *
+ * @class MqttClient
  */
-export class MqttClient {
+export class MqttClient extends EventEmitter {
+  constructor() {
+    super();
+  }
+
+  /**
+   * Sets the identity for connection.
+   *
+   * Certificates and a key constituting the identity is stored in a device
+   * specific keystore.
+   *
+   * @function setIdentity
+   *
+   * @param params
+   *
+   *   Parameters constituting an identity.
+   *
+   * @return Promise<void>
+   *
+   *   Resolved when the identity is set.
+   */
+  setIdentity(params: IdentityParameters) {
+    return MqttClientImpl.setIdentity(params);
+  }
+
   /**
    * Connects to an MQTT broker.
+   *
+   * @function connect
    */
   connect(
-    options: MqttClientOptions,
+    params: ConnectionParameters,
     errorCallback?: CallbackFunction,
     successCallback?: CallbackFunction
   ) {
-    MqttClientImpl.connect(options, errorCallback, successCallback);
+    MqttClientImpl.connect(params, errorCallback, successCallback);
   }
 
   /**
    * Disconnects from the MQTT broker.
+   *
+   * @function disconnect
    */
   disconnect() {
     MqttClientImpl.disconnect();
@@ -26,6 +59,8 @@ export class MqttClient {
 
   /**
    * Publishes a given payload to a specified topic.
+   *
+   * @function publish
    */
   publish(topic: string, payload: string, errorCallback?: CallbackFunction) {
     MqttClientImpl.publish(topic, payload, errorCallback);
@@ -38,12 +73,35 @@ export class MqttClient {
 type CallbackFunction = (message: string) => void;
 
 /**
- * Options for `MqttClient`.
+ * Parameters constituting an identity.
+ *
+ * @interface IdentityParameters
  */
-export type MqttClientOptions = {
-  caCert: string;
-  cert: string;
-  key: string;
+export type IdentityParameters = {
+  /**
+   * PEM representating of a CA certificate.
+   *
+   * @member {string} caCertPem
+   */
+  caCertPem: string;
+  /**
+   * PEM representation of a certificate.
+   *
+   * @member {string} certPem
+   */
+  certPem: string;
+  /**
+   * PEM representation of a private key that has signed `certificate`.
+   *
+   * @member {string} keyPem
+   */
+  keyPem: string;
+};
+
+/**
+ * Parameters for connection to an MQTT broker.
+ */
+export type ConnectionParameters = {
   host: string;
   port: string;
   clientId: string;
