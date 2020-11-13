dca1c7ef279dc0145bb8e13413f22bf18fa8d577
Kikuo Emoto <emoto.kc@om.asahi-kasei.co.jp>
2020-11-13 13:31:05 +0900

    feat: update JavaScript module
    - The native module is wrapped in a JavaScript class `MqttClient`.
    - `example/src/App.tsx` is updated to avoid TypeScript errors. I have no
      plan to maintain the example so far.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import MqttClient from 'react-native-mqtt-client';
+// import MqttClient from 'react-native-mqtt-client';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    MqttClient.multiply(3, 7).then(setResult);
+    setResult(0);
+    // MqttClient.multiply(3, 7).then(setResult);
   }, []);
 
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,54 @@
 import { NativeModules } from 'react-native';
 
-type MqttClientType = {
-  multiply(a: number, b: number): Promise<number>;
+const { MqttClient: MqttClientImpl } = NativeModules;
+
+/**
+ * MQTT client.
+ */
+export class MqttClient {
+  /**
+   * Connects to an MQTT broker.
+   */
+  connect(
+    options: MqttClientOptions,
+    errorCallback?: CallbackFunction,
+    successCallback?: CallbackFunction
+  ) {
+    MqttClientImpl.connect(options, errorCallback, successCallback);
+  }
+
+  /**
+   * Disconnects from the MQTT broker.
+   */
+  disconnect() {
+    MqttClientImpl.disconnect();
+  }
+
+  /**
+   * Publishes a given payload to a specified topic.
+   */
+  publish(topic: string, payload: string, errorCallback?: CallbackFunction) {
+    MqttClientImpl.publish(topic, payload, errorCallback);
+  }
+}
+
+/**
+ * Callback function.
+ */
+type CallbackFunction = (message: string) => void;
+
+/**
+ * Options for `MqttClient`.
+ */
+export type MqttClientOptions = {
+  caCert: string;
+  cert: string;
+  key: string;
+  host: string;
+  port: string;
+  clientId: string;
 };
 
-const { MqttClient } = NativeModules;
+const defaultInstance = new MqttClient();
 
-export default MqttClient as MqttClientType;
+export default defaultInstance;
